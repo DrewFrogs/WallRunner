25d1ffadd4d9059810896ea846495db3f5949bdb
Andrew Maklingham <andrewdec2@gmail.com>
2020-02-19 16:08:41 -0500

    inital menu page

--- WallRunner/WallRunner.Android/MainActivity.cs
+++ WallRunner/WallRunner.Android/MainActivity.cs
@@ -15,12 +15,14 @@ namespace WallRunner.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.Tabbar);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
